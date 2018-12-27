e9efcb2b3ae0d31b4c1c9e8654704db169f4fad7
Lucas Contreras <cont.lucas@gmail.com>
2018-12-27 12:14:52 -0300

    test git

--- src/services/index.js
+++ src/services/index.js
@@ -6,10 +6,8 @@ export const hardEndpoint = "http://test.qbst.com.ar/content_hard.json";
 export function get(url) {
     return axios.get(url)
         .then(result => {
-
             const { data } = result;
-            debugger;
-
+            
             if (data && data.Success == false) {
                 throw data.Error;
             }
@@ -18,9 +16,6 @@ export function get(url) {
         })
         .then(result => result)
         .catch(error => {
-            debugger;
-
-            console.log(error);
             throw error;
         });
 }
